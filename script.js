4cfeb7523bbecf85117214288f820d01680e9738
hkgamer350-hue <hkgamer350@gmail.com>
2026-07-26 13:59:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-const menuToggle = document.getElementById("menu-toggle");
-const menuIcon = document.querySelector(".menu-icon");
 const menuLinks = document.querySelectorAll(".menu a");
+const menuToggle = document.getElementById("menu-toggle");
 
-if (menuIcon && menuToggle) {
-    menuIcon.addEventListener("click", function () {
-        menuToggle.checked = !menuToggle.checked;
-    });
-}
-
-menuLinks.forEach(function(link) {
-    link.addEventListener("click", function() {
-        menuToggle.checked = false;
-    });
+menuLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    menuToggle.checked = false;
+  });
 });
