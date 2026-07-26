b1676136dd136e808977b254944fe529469f4d33
hkgamer350-hue <hkgamer350@gmail.com>
2026-07-26 13:19:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-// Mobile Menu Toggle
 const menuToggle = document.getElementById("menu-toggle");
+const menuIcon = document.querySelector(".menu-icon");
 const menuLinks = document.querySelectorAll(".menu a");
+
+menuIcon.addEventListener("click", () => {
+    menuToggle.checked = !menuToggle.checked;
+});
 
 menuLinks.forEach(link => {
     link.addEventListener("click", () => {
